7d738a8ecbb9db8956bcf340635a42294803c1db
devadarshh <imadarshsingh2002@gmail.com>
2025-09-01 06:55:03 +0530

    feat: setup zod

---
+++ packages/zod/src/schema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+const registerSchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: "Name must be at least 3 characters long." }),
+  email: z.string().email({ message: "Invalid email address." }),
+  password: z
+    .string()
+    .min(3, { message: "Password must be at least 3 characters long." }),
+});
