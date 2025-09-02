4f61b853365877c9dad4e2e862ecbfb92f73fde4
devadarshh <imadarshsingh2002@gmail.com>
2025-09-02 10:19:53 +0530

    feat: JOIN_ROOM added in ws backend

--- apps/ws/src/index.ts
+++ apps/ws/src/index.ts
@@ -1,4 +1,5 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
+import { prisma } from "@repo/db/prisma";
 import { jwt } from "@repo/jwt";
 import dotenv from "dotenv";
 dotenv.config();
@@ -10,7 +11,7 @@ type User = {
   userId: string;
   userName: string;
   ws: WebSocket;
-  roomId: string;
+  roomId: string | null;
 };
 
 type Room = {
@@ -19,6 +20,12 @@ type Room = {
   admin: string;
   createdAt: number;
 };
+
+type ClientMessage = { type: "JOIN_ROOM"; roomId: string };
+
+type ServerMessage =
+  | { type: "ROOM_JOINED"; roomId: string }
+  | { type: "ERROR"; message: string };
 
 const users: Map<string, User> = new Map();
 const rooms: Map<string, Room> = new Map();
@@ -36,7 +43,6 @@ const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 function authUser(token: string) {
   try {
     const decoded = jwt.verify(token, JWT_SECRET_KEY) as JwtPayload;
-    console.log("Decoded token on websocket server:", decoded);
 
     if (!decoded.id) {
       console.error("No valid user ID in token");
@@ -63,8 +69,10 @@ wss.on("connection", (ws, req) => {
   }
   const queryParams = new URLSearchParams(url?.split("?")[1]);
   const token = queryParams.get("token");
-  if (!token) {
-    console.error("No Valid Token Found");
+  const initialRoomId = queryParams.get("room");
+
+  if (!token || !initialRoomId) {
+    console.error("No Valid Token or Room ID Found");
     ws.close();
     return;
   }
@@ -75,4 +83,51 @@ wss.on("connection", (ws, req) => {
     ws.close(1008, "user not authenticated!");
     return;
   }
+
+  // store user in memory
+  const user: User = {
+    userId: userData.userId,
+    userName: userData.userName,
+    ws,
+    roomId: null,
+  };
+  users.set(user.userId, user);
+
+  ws.on("message", async (data) => {
+    const parsedData: ClientMessage = JSON.parse(data.toString());
+
+    if (parsedData.type === "JOIN_ROOM") {
+      const isRoomExist = await prisma.room.findUnique({
+        where: { id: parsedData.roomId },
+      });
+
+      if (!isRoomExist) {
+        console.error("No Room Id Found!");
+        ws.send(
+          JSON.stringify({ type: "ERROR", message: "Room does not exist" })
+        );
+        return;
+      }
+
+      let room = rooms.get(parsedData.roomId);
+
+      if (!room) {
+        room = {
+          roomId: parsedData.roomId,
+          users: [],
+          admin: user.userId,
+          createdAt: Date.now(),
+        };
+        rooms.set(parsedData.roomId, room);
+      }
+
+      room.users.push(user);
+      user.roomId = parsedData.roomId;
+
+      ws.send(
+        JSON.stringify({ type: "ROOM_JOINED", roomId: parsedData.roomId })
+      );
+      console.log(`${user.userName} joined room ${parsedData.roomId}`);
+    }
+  });
 });
