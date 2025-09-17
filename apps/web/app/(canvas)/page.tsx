ec4b24d8fbf9abafb75ae8c7c44a655ef0ec608e
devadarshh <imadarshsingh2002@gmail.com>
2025-09-17 07:20:08 +0530

    feat: use lazy loaded canvasBoard

--- apps/web/app/(canvas)/page.tsx
+++ apps/web/app/(canvas)/page.tsx
@@ -1,13 +1,25 @@
 "use client";
 
 import dynamic from "next/dynamic";
-
-import CanvasBoard from "@/components/canvas/CanvasBoard";
 import { useFirstVisit } from "@/hooks/useFirstVisit";
 
 const WelcomeOverlay = dynamic(
   async () =>
     (await import("@/components/canvas/WelcomeOverlay")).WelcomeOverlay,
+  {
+    ssr: false,
+    loading: () => (
+      <div className="absolute inset-0 flex items-center justify-center z-50 bg-white/5 backdrop-blur-sm">
+        <p className="text-lg font-semibold text-[#8d8bd6]">
+          Loading overlay...
+        </p>
+      </div>
+    ),
+  }
+);
+
+const CanvasBoard = dynamic(
+  async () => await import("@/components/canvas/CanvasBoard"),
   {
     ssr: false,
     loading: () => (
